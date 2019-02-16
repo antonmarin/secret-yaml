aeaa22c959b8e9423e4d676432b970d7ae42dfc8
Anton Marin <antonaryo85@gmail.com>
2019-02-16 11:09:45 +0300

    root ok

--- cmd/root.go
+++ cmd/root.go
@@ -11,12 +11,8 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "syml",
 	Short: "Tool to secret yaml values",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long: `Tool to secret yaml values. 
+Made to be quickly installed right in your ci pipeline`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -26,10 +22,4 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
