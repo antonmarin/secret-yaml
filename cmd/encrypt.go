bae56bd7655bc5b385cc1c98df7961fcbb588379
Anton Marin <antonaryo85@gmail.com>
2019-02-16 12:01:34 +0300

    flag

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -23,14 +23,5 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(encryptCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// encryptCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// encryptCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	encryptCmd.Flags().String("secret", "", "Secret key to encode with")
 }
