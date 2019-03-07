c648fc37cf9f4153d339cf88b7b234ed41e2222e
Anton Marin <antonaryo85@gmail.com>
2019-03-07 08:38:37 +0300

    manipulator draft

--- documentManipulator/yamlManipulator.go
+++ documentManipulator/yamlManipulator.go
@@ -3,8 +3,12 @@ package documentManipulator
 import (
 	"bytes"
 	"crypto/aes"
+	"crypto/rand"
+	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"io"
 )
 
 type YamlManipulator struct {
@@ -55,6 +59,10 @@ func (manipulator YamlManipulator) ApplyToLeafs(callback func([]byte) ([]byte, e
 
 		return result, nil
 	default:
+		//genData, err := manipulator.parseValueOfLeaf([]byte(fmt.Sprintf("%v", data)))
+		//if genData == nil {
+		//	return nil, err
+		//}
 		result, err := callback([]byte(fmt.Sprintf("%v", data)))
 		if err != nil {
 			return nil, err
@@ -64,19 +72,108 @@ func (manipulator YamlManipulator) ApplyToLeafs(callback func([]byte) ([]byte, e
 	}
 }
 
+func (manipulator YamlManipulator) parseValueOfLeaf([]byte) ([]byte, error) {
+	return []byte{}, nil
+}
+
 func pad(data []byte) []byte {
 	padding := aes.BlockSize - len(data)%aes.BlockSize
 	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
 	return append(data, padtext...)
 }
 
-func unpad(data []byte) ([]byte, error) {
-	length := len(data)
-	unpadding := int(data[length-1])
+func Generate(data []byte) ([]byte, error) {
+	dataToEncrypt := pad([]byte(data))
 
-	if unpadding > length {
-		return nil, fmt.Errorf("inconsistent data, unpad failed")
+	cipherData := make([]byte, aes.BlockSize+len(dataToEncrypt))
+	iv := cipherData[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
 	}
 
-	return data[:(length - unpadding)], nil
+	//mode := cipher.NewCBCEncrypter(cipher.Block, iv)
+	//mode.CryptBlocks(cipherData[aes.BlockSize:], dataToEncrypt)
+
+	ivSize := make([]byte, 2)
+	binary.LittleEndian.PutUint16(ivSize, aes.BlockSize)
+
+	var args []byte
+	args = append(args, ivSize...)
+	args = append(args, cipherData...)
+
+	result := make([]byte, hex.EncodedLen(len(args)))
+	hex.Encode(result, args)
+
+	return result, nil
 }
+
+//func (s *AesSecret) Extract(data []byte) ([]byte, error) {
+//	if len(data) == 0 {
+//		return data, nil
+//	}
+//
+//	dataToExtract, err := hexToBinary(data)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	ivLengthInfoSize := 2
+//	ivSize := aes.BlockSize
+//	paddingMaxSize := aes.BlockSize
+//	minimalDataBinarySize := ivLengthInfoSize + ivSize + paddingMaxSize
+//	minimalDataSize := minimalDataBinarySize * 2
+//	if len(dataToExtract) < minimalDataBinarySize { // iv + padding
+//		return nil, fmt.Errorf("minimum required data length: '%v'", minimalDataSize)
+//	}
+//
+//	iv := dataToExtract[ivLengthInfoSize : ivLengthInfoSize+ivSize]
+//	cipherText := dataToExtract[ivLengthInfoSize+ivSize:]
+//
+//	if len(cipherText)%aes.BlockSize != 0 {
+//		return nil, fmt.Errorf("data isn't a multiple of the block size")
+//	}
+//
+//	mode := cipher.NewCBCDecrypter(s.CipherBlock, iv)
+//	mode.CryptBlocks(cipherText, cipherText)
+//
+//	result, err := unpad(cipherText)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	return result, nil
+//}
+//
+//func unpad(data []byte) ([]byte, error) {
+//	length := len(data)
+//	unpadding := int(data[length-1])
+//
+//	if unpadding > length {
+//		return nil, fmt.Errorf("inconsistent data, unpad failed")
+//	}
+//
+//	return data[:(length - unpadding)], nil
+//}
+//func hexToBinary(data []byte) ([]byte, error) {
+//	result := make([]byte, hex.DecodedLen(len(data)))
+//	if _, err := hex.Decode(result, data); err != nil {
+//		return nil, err
+//	}
+//
+//	return result, nil
+//}
+//
+//func IsExtractDataError(err error) bool {
+//	dataErrorPrefixs := []string{
+//		"minimum required data length",
+//		"encoding/hex: odd length hex string",
+//	}
+//
+//	for _, prefix := range dataErrorPrefixs {
+//		if strings.HasPrefix(err.Error(), prefix) {
+//			return true
+//		}
+//	}
+//
+//	return false
+//}
--- documentManipulator/yamlManipulator_test.go
+++ documentManipulator/yamlManipulator_test.go
@@ -1,6 +1,10 @@
 package documentManipulator
 
-import "testing"
+import (
+	"bytes"
+	"gopkg.in/yaml.v2"
+	"testing"
+)
 
 //noinspection GoUnusedParameter
 func returnStatic([]byte) ([]byte, error) {
@@ -8,33 +12,59 @@ func returnStatic([]byte) ([]byte, error) {
 }
 func TestYamlManipulator_ApplyToLeafs(t *testing.T) {
 	manipulator := NewYamlManipulator()
-	var expectedData interface{}
-	var outputData interface{}
+	var expectedData []byte
 	var err error
 
-	simpleData := `---
-someKey: someValue`
-	expectedData = `---
-someKey: staticValue`
-	outputData, err = manipulator.ApplyToLeafs(returnStatic, simpleData)
+	simpleData := `someKey: someValue`
+	expectedData = []byte("someKey: staticValue\n")
+
+	document := make(yaml.MapSlice, 0)
+	err = yaml.Unmarshal([]byte(simpleData), &document)
 	if err != nil {
 		t.Errorf("Should not error on valid yaml. Error: %s", err)
-	}
-	if expectedData != outputData {
-		t.Errorf("Should apply function to leafs of yaml tree.\nExpected: %s\nActual: %s", expectedData, outputData)
 	}
 
-	nestedData := `---
-someKey: 
-  nestedKey: someValue`
-	expectedData = `---
-someKey: 
-  nestedKey: staticValue`
-	outputData, err = manipulator.ApplyToLeafs(returnStatic, nestedData)
+	outputData, err := manipulator.ApplyToLeafs(returnStatic, document)
 	if err != nil {
 		t.Errorf("Should not error on valid yaml. Error: %s", err)
 	}
-	if expectedData != outputData {
-		t.Errorf("Should apply function to leafs of yaml tree.\nExpected: %s\nActual: %s", expectedData, outputData)
+	outputDataBytes, err := yaml.Marshal(outputData)
+	if err != nil {
+		t.Errorf("Should not error on valid yaml. Error: %s", err)
 	}
+
+	if !bytes.Equal(expectedData, outputDataBytes) {
+		t.Errorf("Should apply function to leafs of yaml tree.\nExpected:\n%s\nActual:\n%s", expectedData, outputData)
+	}
+
+	//	nestedData := `---
+	//someKey:
+	//  nestedKey: someValue`
+	//	expectedData = `---
+	//someKey:
+	//  nestedKey: staticValue`
+	//	outputData, err = manipulator.ApplyToLeafs(returnStatic, nestedData)
+	//	if err != nil {
+	//		t.Errorf("Should not error on valid yaml. Error: %s", err)
+	//	}
+	//	if expectedData != outputData {
+	//		t.Errorf("Should apply function to leafs of yaml tree.\nExpected: %s\nActual: %s", expectedData, outputData)
+	//	}
+
+	//	nestedData := `---
+	//key:
+	//  nestedKey: valueEncryptedWithSecretAsdf`
+	//	resultOfEncrypt, err = useCase.Execute(secret, nestedData)
+	//	if err != nil {
+	//		t.Errorf("Should not fail if nestedData valid. Error: %s", err)
+	//	}
+	//	expectedData = `---
+	//key:
+	//  nestedKey: valueEncryptedWithSecretAsdf`
+	//	if expectedData != resultOfEncrypt {
+	//		t.Errorf("Should encrypt only values of nestedData.\nExpected: %s\nGot: %s", expectedData, resultOfEncrypt)
+	//	}
+
+	//	Should return error if yaml invalid
+
 }
