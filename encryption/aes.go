78360d256101dbfebdc51303207e3c40f5481338
Anton Marin <antonaryo85@gmail.com>
2019-03-08 14:25:54 +0300

    AesEncryption

--- encryption/aes.go
+++ encryption/aes.go
@@ -1,13 +1,53 @@
 package encryption
 
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
 type AesEncryptionService struct {
-	secret string
+	secret []byte
 }
 
-func (AesEncryptionService) Encrypt([]byte) ([]byte, error) {
-	panic("implement me")
+func NewAesEncryptionService(secret string) (*AesEncryptionService, error) {
+	if len(secret) != 32 {
+		return nil, fmt.Errorf("secret should be 32 byte length")
+	}
+
+	return &AesEncryptionService{secret: []byte(secret)}, nil
 }
 
-func NewAesEncryptionService(secret string) *AesEncryptionService {
-	return &AesEncryptionService{secret: secret}
+func (service AesEncryptionService) Encrypt(data []byte) ([]byte, error) {
+	block, _ := aes.NewCipher(service.secret)
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	cipherText := gcm.Seal(nonce, nonce, data, nil)
+	return cipherText, nil
+}
+
+func (service AesEncryptionService) Decrypt(data []byte) ([]byte, error) {
+	block, err := aes.NewCipher(service.secret)
+	if err != nil {
+		return nil, err
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	nonceSize := gcm.NonceSize()
+	nonce, cipherText := data[:nonceSize], data[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
 }
--- encryption/aes_test.go
+++ encryption/aes_test.go
@@ -1,12 +1,41 @@
 package encryption
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
-func TestAesEncryptionService_Encrypt(t *testing.T) {
-	//service := NewAesEncryptionService()
-	//
-	//sourceData := []byte("someValue")
-	//secret := "someSecret"
-	//
-	//encryptedData := service.Encrypt()
+func TestNewAesEncryptionService_ShouldCreateWith32byteKey(t *testing.T) {
+	_, err := NewAesEncryptionService("fbd7908e8ccdf67d472995a5c54ea72f")
+	if err != nil {
+		t.Errorf("Should not error with valid secret. Error: %s", err)
+	}
+}
+
+func TestNewAesEncryptionService_ShouldFailWithNot32byteKey(t *testing.T) {
+	secret := "someBadSecret"
+	_, err := NewAesEncryptionService(secret)
+	if err == nil {
+		t.Errorf("Should error on not valid secret. Secret was: %s", secret)
+	}
+}
+
+func TestAesEncryptionService_DecryptedShouldEqualSource(t *testing.T) {
+	//noinspection SpellCheckingInspection
+	service, _ := NewAesEncryptionService("fbd7908e8ccdf67d472995a5c54ea72f")
+
+	sourceData := []byte("someValue")
+
+	encryptedData, err := service.Encrypt(sourceData)
+	if err != nil {
+		t.Errorf("Should not error on valid value. Error: %s", err)
+	}
+	if bytes.Equal(encryptedData, sourceData) {
+		t.Errorf("Encrypted data can't equal source.\nEncrypted: %s\nSource: %s", encryptedData, sourceData)
+	}
+
+	decryptedData, err := service.Decrypt(encryptedData)
+	if !bytes.Equal(sourceData, decryptedData) {
+		t.Errorf("Descrypted should equal source.\nSource: %s\nDecrypted: %s", sourceData, decryptedData)
+	}
 }
